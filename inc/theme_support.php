3a54f810cf8f6965d568cc21e735f2be16e4009b
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2019-03-23 11:41:41 +0000

    Added basic support for backend featured images

---
+++ inc/theme_support.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which
+ * runs before the init hook. The init hook is too late for some features, such
+ * as indicating support for post thumbnails.
+ */
+function wp_rest_theme_setup() {
+
+	/*
+	 * Enable support for Post Thumbnails on posts and pages.
+	 *
+	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+	 */
+	add_theme_support( 'post-thumbnails' );
+
+}
+add_action( 'after_setup_theme', 'wp_rest_theme_setup' );
