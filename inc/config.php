42c5935f2b3913be73e2b5bf787ae083e713c65e
Robert Marshall <16778244+robmarshall@users.noreply.github.com>
2019-11-15 10:12:57 +0000

    Added text domain function

--- inc/config.php
+++ inc/config.php
@@ -1,5 +1,25 @@
 <?php
 
+/**
+ * Set the URL of the live Gatsby site
+ *
+ * @return string The domain of the live site
+ */
+
 function rest_theme_url_redirect() {
     return 'thoughtsandstuff.com';
+}
+
+/**
+ * Get the text domain from the stylesheet
+ *
+ * May seem pointless, but it avoids any
+ * errors from changes later in the day
+ *
+ * @return string The text domain of the theme
+ */
+
+function theme_text_domain(){
+    $theme_details = wp_get_theme();
+    return $theme_details->get('TextDomain');
 }
